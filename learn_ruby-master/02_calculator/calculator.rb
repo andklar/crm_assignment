3f40cf05196bee04e91065843ee40267f23ecaca
Andrew Klar <andrewklar@hotmail.com>
2016-06-08 12:59:27 -0400

    finished calculator

--- learn_ruby-master/02_calculator/calculator.rb
+++ learn_ruby-master/02_calculator/calculator.rb
@@ -9,3 +9,15 @@ end
 def sum(array)
   array.reduce(0) { |sum, x| sum += x }
 end
+
+def multiply(x, y)
+  x * y
+end
+
+def product(array)
+  array.reduce(:*)
+end
+
+def factorial(n)
+  (1..n).reduce(1, :*)
+end
